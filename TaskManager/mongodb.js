3e752a081c0940ab2904e7e9b3746ef2408ad1bf
Ayren Killman <ayrenkillman@gmail.com>
2025-02-04 12:47:56 -0600

    more mongo fun stuff

---
+++ TaskManager/mongodb.js
@@ -0,0 +1,15 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, {}, (error) => {
+    if (error) {
+        return console.log("unable to connect")
+    }
+
+
+
+    console.log('Connected to MongoDB')
+})
