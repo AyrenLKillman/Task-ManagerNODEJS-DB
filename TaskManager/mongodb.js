d180a739a4854a2bd78e8276d7d42d7cec2b4ef1
Ayren Killman <ayrenkillman@gmail.com>
2025-02-10 12:07:58 -0600

    fghjkjgfds

--- TaskManager/mongodb.js
+++ TaskManager/mongodb.js
@@ -11,5 +11,39 @@ MongoClient.connect(connectionURL,{}, (error, client) => {
 
 
 
-    console.log('Connected to MongoDB')
+    const db = client.db(databaseName)
+
+//     db.collection('users').insertOne({
+//         name: 'bob',
+//         age: 21
+//     }, (error, result) => {
+//         if (error) {
+//             return console.log('unable to insert user')
+//         }
+
+//         console.log(result.ops)
+//     })
+
+
+    db.collection('users').insertMany([
+        {
+            name: 'bobby',
+            age: 41
+        },
+        {
+            name: 'joe',
+            age:1
+        }
+    ],(error, result) => {
+        if (error) {
+            return console.log('unable to insert user')
+        }
+
+        console.log(result.ops)
+
+
+    })
 })
+
+//! vid 7
+
