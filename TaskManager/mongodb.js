98062051ac822d045caaa1bbac73a6844a433180
Ayren Killman <ayrenkillman@gmail.com>
2025-02-10 14:15:12 -0600

    Fixing old stuff yay

--- TaskManager/mongodb.js
+++ TaskManager/mongodb.js
@@ -4,46 +4,31 @@ const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL,{}, (error, client) => {
-    if (error) {
-        return console.log("unable to connect")
+const client = new MongoClient(connectionURL);
+
+async function run() {
+    try {
+      await client.connect();
+        //operation stuff
+
+        const db = client.db(databaseName)
+
+        db.collection('users').insertOne({
+            name: 'bob',
+            age: 69
+        })
+
+
+
+    } catch (error){
+        if(error) {
+            return console.log('failed to connect :(')
+        }
+    } finally {
+      await client.close();
     }
-
-
-
-    const db = client.db(databaseName)
-
-//     db.collection('users').insertOne({
-//         name: 'bob',
-//         age: 21
-//     }, (error, result) => {
-//         if (error) {
-//             return console.log('unable to insert user')
-//         }
-
-//         console.log(result.ops)
-//     })
-
-
-    db.collection('users').insertMany([
-        {
-            name: 'bobby',
-            age: 41
-        },
-        {
-            name: 'joe',
-            age:1
-        }
-    ],(error, result) => {
-        if (error) {
-            return console.log('unable to insert user')
-        }
-
-        console.log(result.ops)
-
-
-    })
-})
+  }
+  run().catch(console.dir);
 
 //! vid 7
 
