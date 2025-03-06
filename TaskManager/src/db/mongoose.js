d66312d0ede434e8c9d477c27fa62d380c3f03f1
Ayren Killman <ayrenkillman@gmail.com>
2025-03-06 13:24:51 -0600

    mongoose
    honk

--- TaskManager/src/db/mongoose.js
+++ TaskManager/src/db/mongoose.js
@@ -1,28 +1,57 @@
 const mongoose = require('mongoose')
+const validator = require("validator")
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 })
 
-const User = mongoose.model('user', {
-    name: {
-        type: String
+const Task = mongoose.model('task', {
+    Task: {
+        type: String,
+        required: true
     },
-    age: {
-        type: Number
+    Finished: {
+        type: Boolean,
+        default: false
+    },
+    email:{
+        type: String,
+        required:true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email not valid')
+            }
+        }
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes("password")) {
+                throw new Error('password cant have "password" in it')
+            }
+        }
+    },
+    description:{
+        type:String,
+        required:true,
+        trim: true,
     }
 })
 
-const bob = new User({
-    name:"bob",
-    age:69
+const taskz = new Task({
+    Task: 'walk the fish',
+    email: 'test@chez.com',
+    password: 'swaws39ruw9j',
+    description: "self explanitory"
 })
 
 
-bob.save().then(() => {
-    console.log(bob)
+taskz.save().then(() => {
+    console.log(taskz)
 }).catch((error) => {
-    console.log(error, "error")
+    console.log(error)
 })
-
-//!11.2
+//!11.6 (this is just a massive presentation so dont start server yet)
